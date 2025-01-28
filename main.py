4cbbd0a61ce2c90397b29a13df2b6e6ce995af29
MayukhOfficia <mayukhofficia@gmail.com>
2025-01-28 17:28:35 +0530

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
 from app import create_app
 from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    print("Server is running on http://localhost:8080")
-    serve(app, host='localhost', port=8080)
+    # Retrieve the port dynamically from the PORT environment variable
+    # Default to 8080 if not provided
+    port = int(os.environ.get("PORT", 8080))
+
+    print(f"Server is running on port {port}")
+    # Bind to '0.0.0.0' to allow external access
+    serve(app, host='0.0.0.0', port=port)
